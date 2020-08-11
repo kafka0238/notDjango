c7f44c6f625ebcfb1a3e6de9a7e7b63e742d6d7a
kafka <rachilin@fim.ltd>
2020-08-12 00:30:05 +0300

    first commit

---
+++ notDjango/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'not_django',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 3306,
+    }
+}
